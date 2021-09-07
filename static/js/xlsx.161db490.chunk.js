325c70b9e3271a62681c5b6f898881f83a2647b1
aTNa <atay.narynov@gmail.com>
2021-09-07 15:24:28 +0600

    Updates

---
+++ static/js/xlsx.161db490.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpKloopArticles=this.webpackJsonpKloopArticles||[]).push([[2],{475:function(o,n){},558:function(o,n){},559:function(o,n){}}]);
+//# sourceMappingURL=xlsx.161db490.chunk.js.map
--- static/js/xlsx.cbf2ff5c.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpKloopArticles=this.webpackJsonpKloopArticles||[]).push([[2],{362:function(o,n){},455:function(o,n){},456:function(o,n){}}]);
-//# sourceMappingURL=xlsx.cbf2ff5c.chunk.js.map
